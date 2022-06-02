96ae6f9a83cd50ecbf57dfddc06c95b7c079b0c1
Althea Archer <aaarcher@usgs.gov>
2022-06-02 09:35:46 -0500

    rename functions to be more descriptive

--- run_all_script.R
+++ run_all_script.R
@@ -1,16 +1,31 @@
-
+#' # Test RMSE versus Training Temperature Profiles
+#' 
+#' This script fetches, processes, and plots data to compare Test RMSE versus Training 
+#' temperature profiles. 
+#' 
+#' 
+#' ## Header
+#' 
+#' Load libraries
 library(dplyr)
 library(readr)
 library(stringr)
 library(sbtools)
 library(whisker)
 
+#' Clear environment (optional)
+#' 
 rm(list = ls())
 
-# Source functions
-source(file = '1_fetch/src/functions.R')
-source(file = '2_process/src/functions.R')
-source(file = '3_visualize/src/functions.R')
+
+#' ## Load source functions 
+#' 
+#' Functions to fetch the raw data from Scibase (`fetch()`), process (and save) it for plotting (`process()`), 
+#' and finally create a diagnostic log (`diagnostic_log()`) and associated figure (`line_range_plot()`).
+source(file = '1_fetch/src/fetch_fcn.R')
+source(file = '2_process/src/process_fcn.R')
+source(file = '3_visualize/src/line_range_plot_fcn.R')
+source(file = '3_visualize/src/diagnostic_log_fnc.R')
 
 # 1 Fetch the data by defining sb_id
 sb_id = '5d925066e4b0c4f70d0d0599'
